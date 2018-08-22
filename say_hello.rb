83c8a8d6b9845a35a98158eb5373bcb0fa5078ca
Hanna Strezh <hannastrezh@gmail.com>
2018-08-22 01:17:49 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,5 @@
-def say_hello (phrase ="Hello ", name = "Ruby Programmer!")
-  puts "#{phrase} #{name}"
+def say_hello (phrase ="Hello ", name = "Ruby Programmer")
+  puts "#{phrase} #{name}!"
   
 end
   # Build your say_hello method here
